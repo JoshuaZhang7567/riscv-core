7d1d11ee29cac19904775ca8460702e64684a1d2
SeanInoue <s.i.pewpew.79@gmail.com>
2026-04-17 17:17:35 -0400

    added comments explaining PC

--- PC/pc_register.sv
+++ PC/pc_register.sv
@@ -1,7 +1,12 @@
+/*
+The Program Counter(PC) will hold the instruction address of current and next clock tick. 
+pc_out is outputted and becomes the current address, pc_in is the next address. pc_in is determined by logic on the top level module, such as alu +4 or a jump.
+*/
+
 module pc_register(
     input clk,
     input reset,
-    input [31:0] pc_in,
+    input [31:0] pc_in,//replace [31:0] with word_t
     output reg [31:0] pc_out//reg means constant output, think of a light that is on, shows the current address
 );
 
