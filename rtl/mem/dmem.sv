d8d622abb9621dd1ed9fc5c6602963f3e812e8a1
SeanInoue <s.i.pewpew.79@gmail.com>
2026-06-05 02:07:40 -0400

    Fix: renamed ADDR_WIDTH to RAM_ADDR_WIDTH

--- rtl/mem/dmem.sv
+++ rtl/mem/dmem.sv
@@ -3,7 +3,7 @@ Data Memory Module:
 - Can write or read data with RAM.
 - currently only works with a full word.
 - Next Steps:
-    - input fun3
+    - input func3
     - implement switch case to work with:
         - byte (b)
         - half word (h)
@@ -29,6 +29,7 @@ module dmem
     input logic write_data,
     input logic addr,
 
+    // control signals
     input logic clk,
     input logic mem_write,
     input logic mem_read,
@@ -36,12 +37,12 @@ module dmem
     output logic read_data
 );
     // declare ram
-    localparam DEPTH = 1 << (ADDR_WIDTH-2) // 2 to the power of (ADDR_WIDTH-2); shift binary one left (ADDR_WIDTH-2) bits. Minus 2 to the exponent is essentially divide by 4, because each row is 4 bytes.
+    localparam DEPTH = 1 << (RAM_ADDR_WIDTH-2) // 2 to the power of (ADDR_WIDTH-2); shift binary one left (ADDR_WIDTH-2) bits. Minus 2 to the exponent is essentially divide by 4, because each row is 4 bytes.
     logic [31:0] ram [0:DEPTH-1];
 
     // calculate word addr, since addr is currently byte addr
-    logic [ADDR_WIDTH-2-1:0] word_addr; // 2^32 bytes, so 2^30 words
-    assign word_addr = addr[ADDR_WIDTH-1:2] //drop the last 2 bits, those determine which byte within a word
+    logic [RAM_ADDR_WIDTH-2-1:0] word_addr; // 2^32 bytes, so 2^30 words
+    assign word_addr = addr[RAM_ADDR_WIDTH-1:2] //drop the last 2 bits, those determine which byte within a word
 
     // write data logic
     always_ff @(posedge clk) begin
