c79aeaf754c5b40bb623ce9ad6dd122ab113f2fa
JoshuaZhang7567 <joshuazhang7567@gmail.com>
2026-06-06 02:05:37 -0400

    fix: fixed consistency with riscv_pkg.sv and minor input/output declaration bug in dmem.sv

--- rtl/lib/adder.sv
+++ rtl/lib/adder.sv
@@ -1,5 +1,6 @@
-module adder(input  [31:0] a, b,
-             output [31:0] y);
+module adder #(parameter WIDTH = 32)
+              (input  logic [WIDTH-1:0] a, b,
+               output logic [WIDTH-1:0] y);
 
   assign y = a + b;
 
--- rtl/mem/dmem.sv
+++ rtl/mem/dmem.sv
@@ -23,25 +23,25 @@ import riscv_pkg::*;
 
 module dmem
 #(
-    parameter RAM_ADDR_WIDTH = XLEN
+    parameter ADDR_WIDTH = XLEN
 )
 (
-    input logic write_data,
-    input logic addr,
+    input logic [XLEN-1:0] write_data,
+    input logic [XLEN-1:0] addr,
 
     input logic clk,
     input logic mem_write,
     input logic mem_read,
 
-    output logic read_data
+    output logic [XLEN-1:0] read_data
 );
     // declare ram
-    localparam DEPTH = 1 << (ADDR_WIDTH-2) // 2 to the power of (ADDR_WIDTH-2); shift binary one left (ADDR_WIDTH-2) bits. Minus 2 to the exponent is essentially divide by 4, because each row is 4 bytes.
+    localparam DEPTH = 1 << (ADDR_WIDTH-2); // 2 to the power of (ADDR_WIDTH-2); shift binary one left (ADDR_WIDTH-2) bits. Minus 2 to the exponent is essentially divide by 4, because each row is 4 bytes.
     logic [31:0] ram [0:DEPTH-1];
 
     // calculate word addr, since addr is currently byte addr
     logic [ADDR_WIDTH-2-1:0] word_addr; // 2^32 bytes, so 2^30 words
-    assign word_addr = addr[ADDR_WIDTH-1:2] //drop the last 2 bits, those determine which byte within a word
+    assign word_addr = addr[ADDR_WIDTH-1:2]; //drop the last 2 bits, those determine which byte within a word
 
     // write data logic
     always_ff @(posedge clk) begin
